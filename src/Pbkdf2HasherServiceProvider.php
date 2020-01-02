91b2a86603048d22d832222dbfc80fa76041b4a1
Mark Hofstetter <mark (at) hofstetter.at>
2020-01-02 10:49:10 +0100

    make checking a little more robust

--- src/Pbkdf2HasherServiceProvider.php
+++ src/Pbkdf2HasherServiceProvider.php
@@ -1,17 +1,11 @@
 <?php
 
 namespace MarkHofstetter\Pbkdf2Hasher;
-# namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Hashing\HashServiceProvider;
-use Illuminate\Support\Facades\Hash;
 use Pbkdf2Hasher;
 
-
-# class MarkHofstetter_Pbkdf2Hasher_Pbkdf2HasherServiceProvider extends ServiceProvider
-class Pbkdf2HasherServiceProvider extends ServiceProvider
-# class HashServiceProvider extends ServiceProvider
+class Pbkdf2HasherServiceProvider extends HashServiceProvider
 {
     /**
      * Perform post-registration booting of services.
@@ -20,9 +14,6 @@ class Pbkdf2HasherServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->make('hash')->extend('pbkdf2', function () {
-            return new Pbkdf2Hasher($options = $this->app->config->get('pbkdf2hasher'));
-        });
 
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
@@ -37,7 +28,6 @@ class Pbkdf2HasherServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // print_r($this->app->config->get('hashing.driver'));
         $this->mergeConfigFrom(__DIR__.'/../config/pbkdf2hasher.php', 'pbkdf2hasher');
 
         // Register the service the package provides.
@@ -70,22 +60,5 @@ class Pbkdf2HasherServiceProvider extends ServiceProvider
             __DIR__.'/../config/pbkdf2hasher.php' => config_path('pbkdf2hasher.php'),
         ], 'pbkdf2hasher.config');
 
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/markhofstetter'),
-        ], 'pbkdf2hasher.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/markhofstetter'),
-        ], 'pbkdf2hasher.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/markhofstetter'),
-        ], 'pbkdf2hasher.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
